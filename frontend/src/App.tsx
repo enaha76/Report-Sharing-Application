d538a42625d0034b1ffc04da2e9a3d38c27cdca3
enaha76 <ahmedou.enaha-ext@adias.fr>
2024-11-22 17:40:11 +0000

    set up the next ui components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,45 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
+import {Card, CardFooter,Button} from "@nextui-org/react";
+import { NextUIProvider } from '@nextui-org/react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const handleClick = () => {
+    setCounter(counter + 1);
+  };
+
+  const handleReset = () => {
+    setCounter(0);
+  }
+
   return (
-   <h1>hello</h1>
+    <NextUIProvider>
+      <div className="container">
+     
+ <Card
+      isFooterBlurred
+      radius="lg"
+      className="border-none"
+    >
+      <div className='body'>
+          <h2>{counter}</h2>
+      </div>
+      <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+        <Button onPress={handleReset} className="text-tiny text-white" isDisabled={counter===0}  color="danger" radius="lg" size="sm">
+          reset
+        </Button>
+        <Button onPress={handleClick}  color="primary" radius="lg" size="sm">
+          count
+        </Button>
+      </CardFooter>
+    </Card>
+
+      </div>
+  </NextUIProvider>
   );
 }
 
 export default App;
+
